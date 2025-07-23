c3fb009e1f18a283317cd71de70f5f99c2f17cf6
Gygg <gygginternal@gmail.com>
2025-07-22 22:10:28 -0400

    Legal Pages Changes

--- src/pages/Legalpages/PrivacyPolicyPage.jsx
+++ src/pages/Legalpages/PrivacyPolicyPage.jsx
@@ -87,8 +87,24 @@ function PrivacyPolicyPage() {
         className={styles.legalContent}
         dangerouslySetInnerHTML={{ __html: privacyContent }}
       />
-      <div className={styles.footerLink}>
-        <Link to="/">Back to Home</Link>
+      <div className={styles.footerActions}>
+        <Link to="/" className={styles.backToHomeButton}>
+          <svg
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={styles.backIcon}
+          >
+            <path d="m12 19-7-7 7-7"/>
+            <path d="M19 12H5"/>
+          </svg>
+          Back to Home
+        </Link>
       </div>
     </div>
   );
